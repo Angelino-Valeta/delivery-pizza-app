e105ede78833a4e3b5e98483cb6ac4ca6872ad5c
Angelino Valeta <angelinovaleta@gmail.com>
2021-11-12 13:20:36 +0100

    EndPoint for get Pizzas

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const express = require('express');
 const db = require('./db');
 
 const app = express();
+const Pizza = require('./models/pizzaModel');
 
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
+app.get('/getPizzas', (req, res) => {
+  Pizza.find({}, (err, docs) => {
+    if(err) throw err;
+
+    res.send(docs);
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Welcome');
 })
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log('Server Work ' + port ));
