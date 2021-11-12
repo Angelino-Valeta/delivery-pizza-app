82ec18bbd1be2f3c103383896c9aa55a56a9f086
Angelino Valeta <angelinovaleta@gmail.com>
2021-11-12 13:45:12 +0100

    Pizza Route get All Pizzas

---
+++ routes/pizzasRoute.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const Pizza = require("../models/pizzaModel");
+
+router.get("/getAllPizzas", async (req, res) => {
+  try {
+		
+	const pizzas = await Pizza.find();
+
+	res.status(200).json({
+		data: pizzas
+	})
+     
+  } catch (error) {
+    return res.status(400).json({
+      message: error,
+    });
+  }
+});
+
+module.exports = router;
