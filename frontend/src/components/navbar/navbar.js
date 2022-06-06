5ce05c50fee0e91bf8b3109e6cebcbcfbd7db912
Angelino Valeta <angelinovaleta@gmail.com>
2022-06-06 22:01:46 +0100

    hotfix

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -8,17 +8,18 @@ export default function Navbar(){
           <a className="navbar-brand" href="#">Honline Pizza</a>
           <button className="navbar-toggler"
               type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
+              data-toggle="collapse"
+              data-target="#navbarNav"
               aria-controls="navbarNav"
               aria-expanded="false"
-              aria-label="Toggle navigation">
+              aria-label="Toggle navigation"
+              >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav ml-auto">
               <li className="nav-item text">
-                <a className="nav-link" aria-current="page" href="#">Login</a>
+                <a className="nav-link" href="#">Login</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">Cart</a>
