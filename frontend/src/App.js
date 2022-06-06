d68d19f14a5a4b0f7811d9217bb26eca5dfc58bb
Angelino Valeta <angelinovaleta@gmail.com>
2022-06-06 22:01:25 +0100

    Get bootstart min

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/navbar/navbar';
 
 function App() {
