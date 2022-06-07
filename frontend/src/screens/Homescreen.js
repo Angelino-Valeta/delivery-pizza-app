92d65cfc57c24ef8e0a4cf34bfe64c93aa23a513
Angelino Valeta <angelinovaleta@gmail.com>
2022-06-07 23:02:26 +0100

    Add screen Home

---
+++ frontend/src/screens/Homescreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import pizzas from '../pizzasdata';
+import Pizza from "../components/Pizza";
+
+export default function Homescreen() {
+	return (
+		<div className="row">
+			{ pizzas.map(pizza => {
+				return <div className="col-md-4">
+					<div>
+							<Pizza pizza={pizza}/>
+					</div>
+				</div>
+			}) }
+		</div>
+	)
+}
